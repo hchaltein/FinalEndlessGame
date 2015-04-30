908d3aa69a98ce509191fc4ea269e99ed193dd5d
unknown <al2303@OH1375-29.main.ad.rit.edu>
2015-04-30 15:13:23 -0400

    The beginnings of Click and Drag

---
+++ Assets/Scripts/DragHandler.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+	public GameObject blockDragged;
+    Vector3 startPos;
+
+
+	public void OnBeginDrag(PointerEventData eventData){
+
+        blockDragged = gameObject;
+        startPos = transform.position;
+
+	}
+
+	public void OnDrag(PointerEventData eventData){
+
+	}
+
+	public void OnEndDrag(PointerEventData eventData){
+
+	}
+
+}
